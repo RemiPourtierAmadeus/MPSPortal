01abb8b30be2e9a674a74a253f0984f97712ea73
RemiPourtierAmadeus <remi.pourtier@amadeus.com>
2016-03-24 15:13:18 +0100

    Set up back end part

---
+++ BackEnd/app.js
@@ -0,0 +1,22 @@
+var mysql = require("mysql");
+
+// First you need to create a connection to the db
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "jay",
+    password: "jay"
+});
+
+con.connect(function(err){
+    if(err){
+        console.log('Error connecting to Db');
+        return;
+    }
+    console.log('Connection established');
+});
+
+con.end(function(err) {
+    // The connection is terminated gracefully
+    // Ensures all previously enqueued queries are still
+    // before sending a COM_QUIT packet to the MySQL server.
+});
