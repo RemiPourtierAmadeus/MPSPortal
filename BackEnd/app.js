1d08aa30392e5aa178e300be3ef16abef9badfcf
RemiPourtierAmadeus <remi.pourtier@amadeus.com>
2016-03-24 16:47:46 +0100

    Connection to database

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -2,14 +2,16 @@ var mysql = require("mysql");
 
 // First you need to create a connection to the db
 var con = mysql.createConnection({
-    host: "localhost",
-    user: "jay",
-    password: "jay"
+    host: "ncemysqlp4",
+    port: "3315",
+    user: "backend_portal",
+    password: "Amadeus1",
+    database: "MPS_Portal"
 });
 
 con.connect(function(err){
     if(err){
-        console.log('Error connecting to Db');
+        console.log('Error connecting to Db: '+err);
         return;
     }
     console.log('Connection established');
