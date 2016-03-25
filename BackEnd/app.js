de795babf31c7e169b126479b3144249ad37ed27
RemiPourtierAmadeus <remi.pourtier@amadeus.com>
2016-03-25 15:51:08 +0100

    refactoring app

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,42 +1,58 @@
-var mysql = require("mysql");
+var express = require('express'); // Utilisation du module express de nodejs.
+var logger = require('morgan'); // Permet de gérer les logs et la coloration des messages.
+var router = require('./scripts/apiRequests/routes');
+var database = require('./scripts/models/database');
+var bodyParser = require('body-parser');
+var app = express();
 
-// First you need to create a connection to the db
-var con = mysql.createConnection({
-    host: "ncemysqlp4",
-    port: "3315",
-    user: "backend_portal",
-    password: "Amadeus1",
-    database: "MPS_Portal"
+/**
+ * Utilisation du logger en mode développement.
+ */
+app.use(logger('dev'));
+
+/**
+ * Initialiser de la base de donnée.
+ */
+database.initialisationBDD();
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+/**
+ * Permet de définir les autorisations pour les requêtes HTTP.
+ */
+app.use(function(req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*"); // Définit qui a le droit d'appeler le serveur.
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS"); // Définit les méthodes qu'on a le droit d'utiliser.
+    next();
 });
 
-con.connect(function(err){
-    if(err){
-        console.log('Error connecting to Db: '+err);
-        return;
-    }
-    console.log('Connection established');
+/**
+ * Un fixe d'un problème dont je ne comprend pas l'origine ni la solution.
+ * Le problème venait du fait que quand on essayait de <i>POST</i> le naviguateur web envoyait un <i>OPTIONS</i> à la place.
+ */
+app.options('/*', function(req, res){
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,HEAD,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'content-Type,x-requested-with');
+    res.sendStatus(200);
 });
 
-var employee = { user_id: '3',
-    full_name: 'Test',
-    email_address: 'user@test.com',
-    type: 'Admin',
-    password: 'test'};
+/**
+ * Permet de dire que l'on utilise le routeur nommé <i>router</i> pour la l'adresse <i>localhost:3000/</i>
+ */
+app.use('/',router);
 
-con.query('INSERT INTO T_User SET ?', employee, function(err,res){
-    if(err) throw err;
+/**
+ * Permet de créer un serveur qui écoute sur le port 3000.
+ * @type {http.Server}
+ */
+var server = app.listen(3000, function () {
 
-});
+    var host = server.address().address;
+    var port = server.address().port;
 
-con.query('SELECT * FROM T_User',function(err,rows){
-    if(err) throw err;
+    console.log('Example app listening at http://%s:%s', host, port);
 
-    console.log('Data received from Db:\n');
-    console.log(rows);
-});
-
-con.end(function(err) {
-    // The connection is terminated gracefully
-    // Ensures all previously enqueued queries are still
-    // before sending a COM_QUIT packet to the MySQL server.
 });
--- BackEnd/scripts/model/manageUsers.js
+++ BackEnd/scripts/model/manageUsers.js
@@ -1,6 +1,50 @@
+var mysql = require("mysql");
+
+/*exports.getUsers =  function(success,fail){
+
+ };*/
 
 var mysql = require("mysql");
 
-exports.getUsers =  function(success,fail){
+// First you need to create a connection to the db
+var con = mysql.createConnection({
+    host: "ncemysqlp4",
+    port: "3315",
+    user: "backend_portal",
+    password: "Amadeus1",
+    database: "MPS_Portal"
+});
 
+con.connect(function (err) {
+    if (err) {
+        console.log('Error connecting to Db: ' + err);
+        return;
+    }
+    console.log('Connection established');
+});
+
+var employee = {
+    user_id: '3',
+    full_name: 'Test',
+    email_address: 'user@test.com',
+    type: 'Admin',
+    password: 'test'
 };
+
+con.query('INSERT INTO T_User SET ?', employee, function (err, res) {
+    if (err) throw err;
+
+});
+
+con.query('SELECT * FROM T_User', function (err, rows) {
+    if (err) throw err;
+
+    console.log('Data received from Db:\n');
+    console.log(rows);
+});
+
+con.end(function (err) {
+    // The connection is terminated gracefully
+    // Ensures all previously enqueued queries are still
+    // before sending a COM_QUIT packet to the MySQL server.
+});
