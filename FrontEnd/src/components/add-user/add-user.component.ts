5a8d7b8ebccf47fc5c81c1aa0e1a2498d69df26e
RemiPourtierAmadeus <remi.pourtier@amadeus.com>
2016-03-31 16:07:18 +0200

    Adding data bindings into add user frame

--- FrontEnd/src/components/add-user/add-user.component.ts
+++ FrontEnd/src/components/add-user/add-user.component.ts
@@ -3,6 +3,7 @@
  */
 
 import {Component} from 'angular2/core';
+import {UserComponent} from "../user/user.component";
 
 @Component({
     selector: 'add-user',
@@ -10,6 +11,29 @@ import {Component} from 'angular2/core';
     templateUrl: './add-user.component.html',
     styleUrls : ['./add-user.component.css']
 })
+
+/**
+ * Class AddUser
+ */
 export class AddUserComponent {
+
+    public userTypesValues;
+    public websitePartsValues;
+    public reportsValues;
+    public submitted;
+    public user;
+
+    constructor(){
+        this.submitted = false;
+        this.userTypesValues = ["Admin","Operational","Developer","Manager"];
+        this.websitePartsValues = ["Metrics","Performance"]; //TODO: TO use !
+        this.reportsValues = ["Yes","No"]; //TODO: TO use !
+        this.user= new UserComponent("", "", "",
+            "", [""], true);
+    }
+
+    onSubmit(){
+        this.submitted=true;
+    }
 
 }
--- FrontEnd/src/components/user/user.component.ts
+++ FrontEnd/src/components/user/user.component.ts
@@ -1,5 +1,7 @@
 /**
  * Component UserComponent
+ * This component defines the structure of a user. This definition comes from the architecture
+ * built in the database.
  */
 
 import {Component} from 'angular2/core';
@@ -10,16 +12,34 @@ import {Component} from 'angular2/core';
     templateUrl: './user.component.html',
     styleUrls : ['./user.component.css']
 })
+
+/**
+ * Class User
+ */
 export class UserComponent {
+
+    /**
+     * Constructor
+     * @param userId
+     * @param fullName
+     * @param password
+     * @param login : Generated from the full name
+     * @param email
+     * @param type : Admin, Operational, Developer, Manager
+     * @param websitePart : Metrics and/or Performance
+     * @param sendReport : Boolean
+     * @param emailOptional
+     */
     constructor(
-        public userId: number,
         public fullName: string,
-        public password: string,
         public login: string,
         public email: string,
         public type: string,
-        public websitePart: string,
+        public websitePart: Array,
         public sendReport: boolean,
+        public password?: string, /** When a variable has a ? that means this variable is optional
+                                        when we call the constructor**/
+        public userId?: number,
         public emailOptional?: string
     ) {  }
 }
