491c6478996889c94026309f5a2bdffde80e2573
RemiPourtierAmadeus <remi.pourtier@amadeus.com>
2016-04-01 11:46:47 +0200

    Refactoring all manage user service and user list components

--- FrontEnd/src/shared/services/src/manage-users.service.ts
+++ FrontEnd/src/shared/services/src/manage-users.service.ts
@@ -10,33 +10,33 @@ import {UserComponent} from "../../../components/user/user.component";
 @Injectable()
 export class ManageUsersService {
     private users;
-    private serverLink;
+    private _serverLink ='http://ncevc-04296:3500/users';
 
-    constructor(private http:Http) {
-        /*this.users=this.http.get("localhost:3500/users");
-         console.log(this.users);*/
-        this.serverLink = 'http://ncevc-04296:3500/users';
-    }
+    constructor(private http:Http) {}
 
     getUsers() {
         //this.users=this.http.get("localhost:3500/users");
-        return Observable.forkJoin(
+        /*return Observable.forkJoin(
             this.http.get(this.serverLink).map((res:Response)=>
                 res = res.json()
             )
-        );
+        );*/
+        return this.http.get(this._serverLink)
+            .map(res => <UserComponent[]> res.json().data)
+            .do(data => console.log(data))
+            .catch(this.handleError);
     }
 
 
-    addUser(full_name: string): Observable<UserComponent>  {
+    addUser(user_id: string): Observable<UserComponent>  {
 
-        var tmp='{user_id : "5", full_name : "Pierre"}';
-        //let body = JSON.parse(tmp); //stringify({ tmp });
+        //var tmp='{user_id : "5", full_name : "Pierre"}';
+        let body = JSON.stringify({ user_id });
         let headers = new Headers({'Content-Type': 'application/json'});
         let options = new RequestOptions({headers: headers});
 
-        console.log("User data in addUSer service: "+tmp);
-        return this.http.post(this.serverLink, tmp, options).
+        console.log("User data in addUSer service: "+body);
+        return this.http.post(this._serverLink, body, options).
             map( res=><UserComponent> res.json().data).
             catch(this.handleError);
     }
