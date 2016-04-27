21ee6e1091952f6607c64ee35d0d8a516810c523
RemiPourtierAmadeus <remi.pourtier@amadeus.com>
2016-04-27 09:38:18 +0200

    Adding condition to update login component

--- FrontEnd/src/components/connection-content/connection-content.component.ts
+++ FrontEnd/src/components/connection-content/connection-content.component.ts
@@ -18,14 +18,39 @@ export class ConnectionContentComponent {
     public modelForChild;
     public connectionFailed;
     public errorMessage;
+    /**
+     * Current page value contains the following variable: 1,2,3
+     * Each number corresponds to a component:
+     * 1 = Login Component
+     * 2 = Forgot password Component
+     * 3 = Email Address Component
+     * We use it to know which is the page to display
+     * IF the current page value is 0, that means we have redirect the user to
+     * the home page of the application
+     */
+    public currentPageValue;
 
     constructor(){
         this.modelForChild={errorMessage: '', errorRaised: false};
         this.connectionFailed=false;
         this.errorMessage="";
+        this.currentPageValue=1;
     }
 
     handleChildEvent(arg){}
+
+    /**
+     * Function updateCurrentPageValue.
+     * This function update the variable currentPageValue.
+     */
+    updateCurrentPageValue(){
+        if(this.currentPageValue==1 || this.currentPageValue==2){
+            this.currentPageValue++;
+        }
+        else{
+            this.currentPageValue=0;
+        }
+    }
 
     /**
      * Function sendErrorMessage.
@@ -38,6 +63,9 @@ export class ConnectionContentComponent {
         if(message.length>0){
             this.connectionFailed=true;
             this.errorMessage=message;
+        }
+        else{
+            this.updateCurrentPageValue();
         }
     }
 }
