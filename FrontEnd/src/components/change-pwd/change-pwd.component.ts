06b737ea94e940f313e8ce4846ed34e976b0fe25
RemiPourtierAmadeus <remi.pourtier@amadeus.com>
2016-04-27 14:37:42 +0200

    Creation verification pwd controller

--- FrontEnd/src/components/change-pwd/change-pwd.component.ts
+++ FrontEnd/src/components/change-pwd/change-pwd.component.ts
@@ -5,11 +5,14 @@
 import {Component} from 'angular2/core';
 import {ManageUsersService} from "../../shared/services/src/manage-users.service";
 import {UserComponent} from "../user/user.component";
+import {VerificationPwdComponent} from "../verification-pwd/verification-pwd.component";
 
 @Component({
     selector: 'change-pwd',
     moduleId: module.id,
     templateUrl: './change-pwd.component.html',
+    controller: VerificationPwdComponent,
+    controllerAs: "passwordVerification",
     styleUrls : ['./change-pwd.component.css']
 })
 
---
+++ FrontEnd/src/components/verification-pwd/verification-pwd.component.spec.ts
@@ -0,0 +1,11 @@
+/**
+ * Test Component VerificationPwd
+ */
+import {VerificationPwdComponent} from "./verification-pwd.component";
+import {Component} from "angular2/core";
+@Component({
+    selector: 'test-verification-pwd',
+    template: '<sd-verification-pwd></sd-verification-pwd>',
+    directives: []
+})
+class TestVerificationPwdComponent {}
