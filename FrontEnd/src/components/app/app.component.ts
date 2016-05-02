3039dd101feef236683a335bc61e584307af1737
RemiPourtierAmadeus <remi.pourtier@amadeus.com>
2016-05-02 14:41:59 +0200

    Updating design and align component for home page

--- FrontEnd/src/components/app/app.component.ts
+++ FrontEnd/src/components/app/app.component.ts
@@ -66,7 +66,7 @@ export class AppComponent {
     public message="Doesn't work";
 
     constructor(private _manageUserService: ManageUsersService){
-        this.pageToShow=1;
+        this.pageToShow=0;
         this.user = new UserComponent("", "", "",
             "", false, false, false, false, "","");
     }
