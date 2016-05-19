dc415926e8a8ee0c7898b2e07a204711add3881c
RemiPourtierAmadeus <remi.pourtier@amadeus.com>
2016-05-19 14:28:21 +0200

    Integrating news service into component

--- FrontEnd/src/components/home-page/news-model/news-model.component.ts
+++ FrontEnd/src/components/home-page/news-model/news-model.component.ts
@@ -10,12 +10,23 @@ import {Component} from 'angular2/core';
     templateUrl: './news-model.component.html',
     styleUrls : ['./news-model.component.css']
 })
+
+/**
+ * Model for news.
+ * This component contains the characteristics of a news. We use it to store news information
+ * in the front end.
+ */
 export class NewsModelComponent {
 
     constructor(
+        public id:number,
         public title:string,
+        public content:string,
         public date:string,
         public hour:string,
-        public type:string[]
+        public type:string,
+        public subtype:string,
+        public newsFrom:string,
+        public state:string
     ){}
 }
