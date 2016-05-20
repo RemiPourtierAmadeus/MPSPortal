10c7452ace9d08cbc8d0108412be33d9494ff4f3
RemiPourtierAmadeus <remi.pourtier@amadeus.com>
2016-05-20 18:23:16 +0200

    Cleaning

--- FrontEnd/src/components/app/app.component.ts
+++ FrontEnd/src/components/app/app.component.ts
@@ -68,7 +68,7 @@ export class AppComponent {
     public message="Doesn't work";
 
     constructor(private _manageUserService: ManageUsersService){
-        this.pageToShow=3;
+        this.pageToShow=1;
         this.user = new UserComponent("", "", "",
             "", false, false, false, false, "","");
     }
