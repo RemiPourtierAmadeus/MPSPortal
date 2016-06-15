179f5ee8929567110d242c81b12eb151b0705d1c
RemiPourtierAmadeus <remi.pourtier@amadeus.com>
2016-06-15 10:35:17 +0200

    Adding news orderng in the home page

--- FrontEnd/src/app/components/home-page/news-panel/news-panel.component.ts
+++ FrontEnd/src/app/components/home-page/news-panel/news-panel.component.ts
@@ -6,20 +6,22 @@ import {Component} from '@angular/core';
 import {NewsItemComponent} from "../../news/news-item/news-item.component";
 import {NewsModelComponent} from "../../models/news-model/news-model.component";
 import {ManageNewsService} from "../../../shared/services/src/manage-news.service";
+import {NewsConstantService} from "../../../shared/services/src/news-constant.service";
 
 @Component({
     selector: 'news-panel',
     moduleId: module.id,
     templateUrl: './news-panel.component.html',
     styleUrls : ['./news-panel.component.css'],
-    directives: [NewsItemComponent]
+    directives: [NewsItemComponent],
+    providers:[NewsConstantService]
 })
 export class NewsPanelComponent {
 
     newsList: NewsModelComponent[];
     newsNotFound: boolean;
 
-    constructor(private _manageNewsService: ManageNewsService){}
+    constructor(private _manageNewsService: ManageNewsService, private _newsConstantService:NewsConstantService){}
 
     ngOnInit(){
         this.newsNotFound=false;
@@ -35,9 +37,42 @@ export class NewsPanelComponent {
      */
     getNews(){
         this._manageNewsService.getNews().then(
-            news => this.newsList=news,
+            news => this.orderByDate(news),
             error => this.noNews(error)
         );
+    }
+
+    /**
+     * Function orderByDate.
+     * This function sort the news according to the date, from the most recent news to the older.
+     */
+    orderByDate(news){
+        this.newsList=news;
+        let dateSorted=[];
+        dateSorted.push(this.newsList[0]);
+        let diff=0;
+        for(let i=1;i<this.newsList.length;i++){
+            for(let j=0;j<dateSorted.length;j++){
+                diff=this.newsList[i].date.localeCompare(dateSorted[j].date);
+                if(diff==-1|| diff==0){
+                    dateSorted.splice(j,0,this.newsList[i]);
+                    j=dateSorted.length;
+                }
+                if(j+1==dateSorted.length){
+                    dateSorted.push(this.newsList[i]);
+                    j=dateSorted.length;
+                }
+            }
+        }
+        dateSorted.reverse();
+        this.newsList=[];
+        /**
+         * At the end, we keep only a certain number of news (most recent). This number comes from the
+         * NewsConstantService.
+         */
+        for(let i=0;i<this._newsConstantService.getNumberOfLastNews();i++){
+            this.newsList.push(dateSorted[i]);
+        }
     }
 
     /**
