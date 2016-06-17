4d612e7dc36675a212a08eb4f1b27299659eedd5
RemiPourtierAmadeus <remi.pourtier@amadeus.com>
2016-06-17 11:15:55 +0200

    Refactoring component

--- FrontEnd/src/app/components/test-33/index.ts
+++
@@ -1,4 +0,0 @@
-/**
- * This barrel file provides the export for the lazy loaded Test33.
- */
-export * from './test-33.component';
--- FrontEnd/src/app/components/test/test/index.ts
+++
@@ -1,4 +0,0 @@
-/**
- * This barrel file provides the export for the lazy loaded Test.
- */
-export * from './test.component';
