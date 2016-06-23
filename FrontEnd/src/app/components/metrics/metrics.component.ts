282074ac0cb6e38d3a73d537d65efe99be5d9615
RemiPourtierAmadeus <remi.pourtier@amadeus.com>
2016-06-23 14:51:02 +0200

    Metrics service integration

--- FrontEnd/src/app/components/metrics/metrics.component.ts
+++ FrontEnd/src/app/components/metrics/metrics.component.ts
@@ -4,6 +4,7 @@
 
 import {Component} from '@angular/core';
 import {PanelComponent} from "./panel/panel.component";
+import {ManageMetricsService} from "../../shared/services/src/metrics/manage-metrics.service";
 
 @Component({
     selector: 'metrics',
@@ -15,4 +16,21 @@ import {PanelComponent} from "./panel/panel.component";
 export class MetricsComponent {
 
 
+    private metricsItem:string[];
+
+    constructor(private _manageMetricsService:ManageMetricsService){
+    }
+
+    /**
+     * Function ngOnInit.
+     * This function is native with Angular 2. It will be automatically executed
+     * just after the construction of every components of the page.
+     * In the function, we need to initialize the list of metrics item. For security
+     * measure, we wait for construction before calling the function getMetricsItem.
+     */
+    ngOnInit(){
+        this._manageMetricsService.getMetricsItem().then(
+            items => this.metricsItem = items
+        );
+    }
 }
--- FrontEnd/src/app/shared/services/src/manage-metrics.service.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Service ManageMetricsService
- */
-import {Injectable} from '@angular/core';
-
-@Injectable()
-export class ManageMetricsService {
-}
