2454ad8466793bf3aaf2f2f1fda47c756dbdb483
RemiPourtierAmadeus <remi.pourtier@amadeus.com>
2016-06-23 14:39:22 +0200

    adding a mock for metrics panel

---
+++ FrontEnd/src/app/shared/mocks/mock-metrics.ts
@@ -0,0 +1,12 @@
+/**
+ * Mocks METRICSITEM.
+ * We use this mock into first page of the metrics part (metrics component) in order to fill
+ * each m-panel (metrics/panelComponent)
+ * @type {{name: string, pathImage: string, color: string}[]}
+ */
+export var METRICSITEM: string[] = [
+    {name: "News", pathImage: 'Jean', color: '#0063A7'},
+    {name: "Projects & Sessions", pathImage: 'Jean', color: '#D57157'},
+    {name: "Statistics", pathImage: 'Jean', color: '#0DB3B3'},
+    {name: "Settings", pathImage: 'Jean', color: '#5A5A5A'}
+];
