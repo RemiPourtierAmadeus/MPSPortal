0693a0f8534d41286f38ad175d5920dc88abc373
RemiPourtierAmadeus <remi.pourtier@amadeus.com>
2016-06-27 10:19:37 +0200

    Building routes for project

--- BackEnd/scripts/routing/routeProjects.js
+++ BackEnd/scripts/routing/routeProjects.js
@@ -10,7 +10,31 @@ var router = express.Router();
 
 var database = require('../model/database');
 
-var userManager = require('./../model/manageProjects');
+var projectManager = require('./../model/manageProjects');
+
+/**
+ * Function post. When the front-end is requesting a post on
+ * on "nothing" (on "/") we know the user would like to log in.
+ * In consequence, we verify the couple login/password and return the success or the failure of the request
+ * updateUsers from userManager.
+ */
+router.post('/', function(req,res){
+    var success = function (objetJSON) {
+        console.log(objetJSON);
+        res.send(objetJSON);
+    };
+
+    var fail = function(){
+        var finalObject = [{success: 'false'}];
+        res.send(finalObject);
+        //res.sendStatus(500);
+    };
+
+    // Grab data from http request
+    var data = req.body;
+    console.log("In post, data received: "+data);
+
+});
 
 
 /**
