1ced98678bbdf76824f36fced5add4913b3dc23e
RemiPourtierAmadeus <remi.pourtier@amadeus.com>
2016-06-29 11:36:19 +0200

    Delete project component and fixing production tasks

---
+++ FrontEnd/src/app/components/+projects/delete-project/delete-project.component.ts
@@ -0,0 +1,13 @@
+/**
+ * Component ProjectsDeleteProjectComponent
+ */
+
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'delete-project',
+    moduleId: module.id,
+    templateUrl: './delete-project.component.html',
+    styleUrls : ['./delete-project.component.css']
+})
+export class DeleteProjectComponent { }
---
+++ FrontEnd/src/app/components/+projects/delete-project/index.ts
@@ -0,0 +1,4 @@
+/**
+ * This barrel file provides the export for the lazy loaded DeleteProject.
+ */
+export * from './delete-project.component';
