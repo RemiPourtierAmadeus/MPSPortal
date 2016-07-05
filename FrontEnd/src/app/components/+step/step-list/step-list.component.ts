e485b1d7da961626148e523e736d25a00fe5705a
RemiPourtierAmadeus <remi.pourtier@amadeus.com>
2016-07-05 17:01:20 +0200

    Adding steps component

---
+++ FrontEnd/src/app/components/+step/step-list/step-list.component.ts
@@ -0,0 +1,13 @@
+/**
+ * Component StepStepListComponent
+ */
+
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'step-list',
+    moduleId: module.id,
+    templateUrl: './step-list.component.html',
+    styleUrls : ['./step-list.component.css']
+})
+export class StepListComponent { }
