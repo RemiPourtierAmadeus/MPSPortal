86b4ba67c40845b9a66120ec09fa041891010c6b
RemiPourtierAmadeus <remi.pourtier@amadeus.com>
2016-07-21 18:31:51 +0200

    input for project names

--- FrontEnd/src/app/components/+projects/add-project/add-project.component.ts
+++ FrontEnd/src/app/components/+projects/add-project/add-project.component.ts
@@ -4,6 +4,8 @@
 
 import {Component} from '@angular/core';
 import {ShowHideButtonComponent} from "../../core/show-hide-button/show-hide-button.component";
+import {ProjectModel} from "../../models/project.model";
+import {ManageProjectService} from "../../../shared/services/src/manage-project.service";
 
 @Component({
     selector: 'add-project',
@@ -11,14 +13,40 @@ import {ShowHideButtonComponent} from "../../core/show-hide-button/show-hide-but
     templateUrl: './add-project.component.html',
     styleUrls : ['./add-project.component.css'],
     directives: [ShowHideButtonComponent],
+    providers: [ManageProjectService]
 })
 export class AddProjectComponent {
 
     private showForm:boolean;
+    private projects:Array<ProjectModel>;
+    private errorMessage:string;
+    private projectNames:Array<string>;
 
-    constructor(){
-        this.showForm=false;
+    constructor(private manageProjectService:ManageProjectService){
+        this.showForm=true;
+        this.projects=[];
+        this.projectNames=[];
     }
+
+    ngOnInit(){
+        this.manageProjectService.getProjects().then(
+            projects => this.projects=projects,
+            err => this.errorMessage=err
+        );
+    }
+
+    search (term) {
+        if(term.length>0){
+            console.log(term);
+            this.projectNames=[];
+            for(let i=0;i<this.projects.length;i++){
+                if(this.projects[i].name.indexOf(term)==0){
+                    this.projectNames.push(this.projects[i].name);
+                }
+            }
+        }
+    }
+
 
     /**
      * The function showHideForm changes the attribute value of showForm in order to
@@ -26,5 +54,9 @@ export class AddProjectComponent {
      */
     showHideForm(show:boolean){
         this.showForm=show;
+    }
+
+    onSubmit(){
+        //TODO: GET PROJECTS
     }
 }
