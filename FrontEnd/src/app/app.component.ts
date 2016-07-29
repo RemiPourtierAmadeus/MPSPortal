6b3ca4ac7ed9fd5bbd983aa8381d0889b9325adc
RemiPourtierAmadeus <remi.pourtier@amadeus.com>
2016-07-29 17:18:08 +0200

    Preparing to deploy

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -66,7 +66,7 @@ export class AppComponent {
     public routerLinks:string[];
 
     constructor(){
-        this.pageToShow=2;
+        this.pageToShow=0;
         this.user = new UserComponent("", "", "",
             "", false, false, false, false, "","");
         this.instantiateSettings();
